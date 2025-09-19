7ab0e1ed515d1062223fb887b5b99d989810dc09
Arman Hassanniakalager <hassannia@outlook.com>
2025-09-19 13:05:38 +0100

    update

--- sample_script.py
+++ sample_script.py
@@ -7,33 +7,40 @@ This is a temporary script file.
 
 from MLFraud_module import ML_Fraud as mf
 
-#a=mf()
-# a.mc_analysis(adjust_serial='biased')
-# a.mc_analysis(adjust_serial=True)
-# a.mc_analysis(adjust_serial=False)
 
-a=mf(cv_type='kfold',cv_flag=False,adjust_serial=True)
-a.analyse_raw()
-# a.analyse_ratio()
-# a.analyse_fk()
+def _main():
+    # a=mf()
+    # a.mc_analysis(adjust_serial='biased')
+    # a.mc_analysis(adjust_serial=True)
+    # a.mc_analysis(adjust_serial=False)
 
-# a=mf(cv_type='temp',cv_flag=False,adjust_serial=True)
-# a.analyse_raw()
-# a.analyse_ratio()
-# a.analyse_fk()
+    a = mf(cv_type='kfold', cv_flag=False, adjust_serial=True)
+    a.analyse_raw()
+    # a.analyse_ratio()
+    # a.analyse_fk()
 
-# a=mf(cv_type='temp',cv_flag=False,adjust_serial=False)
-# a.analyse_raw()
-# a.analyse_ratio()
-# a.analyse_fk()
+    # a=mf(cv_type='temp',cv_flag=False,adjust_serial=True)
+    # a.analyse_raw()
+    # a.analyse_ratio()
+    # a.analyse_fk()
 
-# a=mf(cv_type='temp',cv_flag=False,adjust_serial=False)
-# a.analyse_raw()
-# a.analyse_ratio()
-# a.analyse_fk()
+    # a=mf(cv_type='temp',cv_flag=False,adjust_serial=False)
+    # a.analyse_raw()
+    # a.analyse_ratio()
+    # a.analyse_fk()
 
-# for gap in [1,2]:
-#     a=mf(OOS_gap=gap)
-#     a.analyse_ratio()
-#     a.analyse_raw()
-#     a.analyse_fk()
+    # a=mf(cv_type='temp',cv_flag=False,adjust_serial=False)
+    # a.analyse_raw()
+    # a.analyse_ratio()
+    # a.analyse_fk()
+
+    # for gap in [1,2]:
+    #     a=mf(OOS_gap=gap)
+    #     a.analyse_ratio()
+    #     a.analyse_raw()
+    #     a.analyse_fk()
+    pass
+
+
+if __name__ == "__main__":
+    _main()
